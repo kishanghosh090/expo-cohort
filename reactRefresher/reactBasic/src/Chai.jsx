b8f53f7e0d8639020bf2a4b5faeddc184f439181
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-03 20:40:34 +0530

    todo test

--- reactRefresher/reactBasic/src/Chai.jsx
+++ reactRefresher/reactBasic/src/Chai.jsx
@@ -2,11 +2,47 @@ import React, { useState } from "react";
 
 function Chai() {
   // const [count, setCount] = useState(0);
-  const [theme, setTheme] = useState("#1a1a1a")
+  // const [theme, setTheme] = useState("#1a1a1a")
+  // const
+  const [task, setTask] = useState([]);
+  const [taskText, setTaskText] = useState("");
+  const [taskDate, setTaskDate] = useState(null);
+
+  function handleTaskText(e) {
+    console.log(e.target.value);
+    setTaskText(e.target.value);
+  }
+  function handleDate(e) {
+    console.log(e.target.value);
+    setTaskDate(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(taskText);
+    console.log(taskDate);
+    console.log(task);
+
+    setTask((prev) => [...prev, { taskText, taskDate }]);
+  }
   return (
     <div className="">
-      {/* <div>Chai aur code {count}</div> */}
-      {/* <button onClick={() => setCount((prev) => prev + 1)}>add</button> */}
+      <input type="text" name="task" id="" onChange={handleTaskText} />
+      <input type="date" name="" id="" onChange={handleDate} />
+      <button onClick={handleSubmit}>Add Task</button>
+      {task.length != 0 ? (
+        <ul>
+          {task.map((element) => {
+            return (
+              <>
+                <li>{element.taskText}</li>
+                <li>{element.taskDate}</li>
+              </>
+            );
+          })}
+        </ul>
+      ) : (
+        <div>no task found</div>
+      )}
     </div>
   );
 }
