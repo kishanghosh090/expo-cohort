4e029e34e709cd1b2d66e3b66dcd484c814a0ad4
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-17 12:28:26 +0530

    route grouping

--- exporouter/src/app/_layout.tsx
+++ exporouter/src/app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    // <SafeAreaView style={{ flex: 1 }}>
+    //   <Text>Header</Text>
+    //   <Slot />
+    //   <Text>Footer</Text>
+    // </SafeAreaView>
+    <Stack />
+  );
 }
--- exporouter/src/components/Home.tsx
+++ exporouter/src/components/Home.tsx
@@ -1,16 +1,34 @@
+// import { Link } from "expo-router";
+// import React from "react";
+// import { StyleSheet, Text, View } from "react-native";
+
+// const Home = () => {
+//   return (
+//     <View>
+//       <Text>Home</Text>
+//       <Link href={"/profile/details"}>About</Link>
+//       <Link href={"/user/kishanranaghosh"}>id</Link>
+//       <Link href={"/username/kishanranaghoshsssssc"}>name</Link>
+//       <Link href={"/docs/ssss/getting-started/23dddsd"}>name</Link>
+//       <Link href={"/newdocs/hello/from/chai/code"}>... catche all</Link>
+//     </View>
+//   );
+// };
+
+// export default Home;
+
+// const styles = StyleSheet.create({});
+
 import { Link } from "expo-router";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+
 
 const Home = () => {
   return (
     <View>
       <Text>Home</Text>
-      <Link href={"/profile/details"}>About</Link>
-      <Link href={"/user/kishanranaghosh"}>id</Link>
-      <Link href={"/username/kishanranaghoshsssssc"}>name</Link>
-      <Link href={"/docs/ssss/getting-started/23dddsd"}>name</Link>
-      <Link href={"/newdocs/hello/from/chai/code"}>... catche all</Link>
+      <Link href={"/about"}>name</Link>
     </View>
   );
 };
