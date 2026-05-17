fe76a4ac0db07ed2bb1474f2f2cf173bbc2fd590
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-17 13:06:25 +0530

    custom tabs

--- exporouter/src/app/(tabs)/_layout.tsx
+++ exporouter/src/app/(tabs)/_layout.tsx
@@ -1,16 +1,40 @@
-import { NativeTabs } from "expo-router/unstable-native-tabs";
+// import { NativeTabs } from "expo-router/unstable-native-tabs";
+
+// export default function TabLayout() {
+//   return (
+//     <NativeTabs>
+//       <NativeTabs.Trigger name="index">
+//         <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+//         <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
+//       </NativeTabs.Trigger>
+//       <NativeTabs.Trigger name="settings">
+//         <NativeTabs.Trigger.Icon sf="gear" md="settings" />
+//         <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
+//       </NativeTabs.Trigger>
+//     </NativeTabs>
+//   );
+// }
+
+import { Tabs } from "expo-router";
+import { View } from "react-native";
+
+function MyTabs({ state, descripter, navigator }) {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        padding: 10,
+      }}
+    ></View>
+  );
+}
 
 export default function TabLayout() {
   return (
-    <NativeTabs>
-      <NativeTabs.Trigger name="index">
-        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
-      </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="settings">
-        <NativeTabs.Trigger.Icon sf="gear" md="settings" />
-        <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
-      </NativeTabs.Trigger>
-    </NativeTabs>
+    <Tabs>
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="about" />
+      <Tabs.Screen name="settings" />
+    </Tabs>
   );
 }
