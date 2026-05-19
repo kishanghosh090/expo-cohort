7c95e5ac870731500d615c99824a53aee8280ecf
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-19 22:45:02 +0530

    add stack nav

--- exporouter/src/app/_layout.tsx
+++ exporouter/src/app/_layout.tsx
@@ -1,39 +1,17 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const isLoggedIn = true;
   return (
-    // <SafeAreaView style={{ flex: 1 }}>
-    //   <Text>Header</Text>
-    //   <Slot />
-    //   <Text>Footer</Text>
-    // </SafeAreaView>
     <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#212121",
-        },
-        headerTintColor: "#fff",
+      screenOptions={({ navigation, route, theme }) => {
+        console.log(navigation);
+        console.log(route);
+        console.log(theme);
+        
+        return {
+          headerTitleStyle: {},
+        };
       }}
-    >
-      <Stack.Protected guard={!isLoggedIn}>
-        <Stack.Screen
-          name="(auth)"
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack.Protected>
-      <Stack.Protected guard={isLoggedIn}>
-        {/* <Stack.Screen
-          name="index"
-          options={{
-            title: "Home",
-          }}
-        />
-        <Stack.Screen name="about" /> */}
-        <Stack.Screen name="(tabs)" />
-      </Stack.Protected>
-    </Stack>
+    />
   );
 }
---
+++ exporouter/src/app/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>hello from chai code</Text>
+      <Link href={"/about"}>about</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
