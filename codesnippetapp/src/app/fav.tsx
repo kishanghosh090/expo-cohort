7775f4603fe7d3b7b9aef2109ad9a6ebb69525c6
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-25 02:12:04 +0530

    start code snippet app

---
+++ codesnippetapp/src/app/fav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const FavScreen = () => {
+  return (
+    <View>
+      <Text>FavScreen</Text>
+    </View>
+  );
+};
+
+export default FavScreen;
+
+const styles = StyleSheet.create({});
---
+++ codesnippetapp/src/libs/expoSqlite.ts
@@ -0,0 +1,8 @@
+function createFolder(folderPath: string | undefined) {}
+function createFileForCodeSnippet(folderPath: string | undefined) {}
+
+function editFolderNameById(id: string) {}
+function editCodeSnippetById(id: string) {}
+
+function deleteFolderById(id: string) {}
+function deleteCodeSnippetById(id: string) {}
