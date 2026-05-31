5006230e9d6c1a2d51148e2ed31d59f4ae6c9fad
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-31 12:04:34 +0530

    add sensors

--- exposensorpart2/src/app/index.tsx
+++ exposensorpart2/src/app/index.tsx
@@ -1,5 +1,18 @@
+import { DeviceMotion } from "expo-sensors";
+import { useEffect } from "react";
 import Compass from "../../magnetometer/components/Compass";
 
 export default function Index() {
+  useEffect(() => {
+    (async () => {
+      const isAvailable = await DeviceMotion.isAvailableAsync();
+
+      console.log(isAvailable);
+
+      if (!isAvailable) {
+        return;
+      }
+    })();
+  }, []);
   return <Compass />;
 }
