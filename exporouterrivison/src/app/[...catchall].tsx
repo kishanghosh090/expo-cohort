5e0e5781d9f1bf135313210d31a9599a6c1fcddb
kishanghosh090 <kishanghosh090@gmail.com>
2026-06-18 18:41:22 +0530

    react router rivison

---
+++ exporouterrivison/src/app/[...catchall].tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const CatchAll = () => {
+  return (
+    <View>
+      <Text>CatchAll</Text>
+    </View>
+  );
+};
+
+export default CatchAll;
+
+const styles = StyleSheet.create({});
